3d387a6044154b32b2ef1c1f244e5d23e4cae29d
allic <allicemarkova@gmail.com>
2025-12-08 22:21:34 +1000

    Словарь с количеством выполненных функций. Выбор аргументов.

--- Count.py
+++ Count.py
@@ -1,11 +1,13 @@
+cnt = {}
+
 def counter(func):
     def wrapper(*args):
-        func.count += 1
-        print(f'Функцию {func.__name__} вызывали {func.count} ')
+        cnt[func.__name__] += 1
+        print(f'Функцию {func.__name__} вызывали {cnt[func.__name__]} ')
         return func(*args)
-    func.count = 0
-    return wrapper
 
+    cnt[func.__name__] = 0
+    return wrapper
 
 @counter
 def f(x, y):
@@ -22,18 +24,10 @@ def h(y):
     return y ** 2
 
 
-print(f(3, 55))
-print(g(2))
-print(g(5))
-print(h(3))
-print(h(12))
-print(h(33))
-
-
 def print_statistics():
     print('Сколько раз вызывались функции: ')
-    for func, count in counter:
-        print(f'{func}: {count} раз')
+    for func_name, count in cnt.items():
+        print(f'{func_name}: {count} раз')
     print()
 
 
@@ -44,19 +38,30 @@ def main():
         '3': h,
         '4': print_statistics
     }
-    if __name__ == "__main__":
-        main()
 
     print('Выберите функцию (1-3) или 4 чтобы узнать, сколько раз вызывались функции ')
 
     while True:
         choice = input('Ваш выбор: ')
-        if choice in func:
-            func[choice]()
-        elif choice == '4':
+        if choice == '4':
             print_statistics()
+        elif choice in func:
+            try:
+                if choice == '1':
+                    x = float(input('Введите значение x: '))
+                    y = float(input('Введите значение y: '))
+                    result = func[choice](x, y)
+                    print(f'Результат: {result}')
+                else:
+                    arg = float(input('Введите значение аргумента: '))
+                    result = func[choice](arg)
+                    print(f'Результат: {result}')
+            except ValueError:
+                print('Необходимо ввести число')
+            except TypeError as e:
+                print(f'Ошибка вызова функции: {e}')
         else:
-            print('Ошибка ввода. Выберите функцию (1-3) или 4 чтобы узнать, сколько раз вызывались функции')
+            print('Ошибка ввода. Выберите функцию 1, 2, 3 или 4.')
 
 
 if __name__ == "__main__":
